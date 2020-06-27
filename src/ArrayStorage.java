0897678e5020f5fb22b20f8497358af596513265
BolshakovNick <67463935+BolshakovNick@users.noreply.github.com>
2020-06-27 14:34:19 +0300

    реализовал: метод clear - обнуление массива метод save - проходится циклом по элементам массива и если элемент массива равен пустой, то сохраняем в него метод delete - ищет элемент по uuid и делает его null, потом смещает все впереди стоящие элементы на один влево. метод getAll - создает новый массив длинной для всех ненулевых элементов и возвращает его метод size - проходит циклом по элементам массива пока они ненулевые и возвращает их количество

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,64 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = null;
     }
 
+    //save the new element after last element
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+            }
+        }
     }
 
+    //get the element
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break;
+            }
+        }
+        return resume;
     }
 
+    //delete element and shift other elements one left
     void delete(String uuid) {
+        int count = size();
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j = i; j < count; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] r = new Resume[this.size()];
+        for (int i = 0; i < size(); i++) {
+            r[i] = storage[i];
+        }
+        return r;
     }
 
+    //not null element length
     int size() {
-        return 0;
+        int size = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                size = i;
+                break;
+            }
+        }
+        return size;
     }
 }
