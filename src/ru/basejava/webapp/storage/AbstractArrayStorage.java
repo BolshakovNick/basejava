4151997a5b157ff16abd7579217e28ce31cbff32
Local <bolshakov_wrestling@icloud.com>
2020-07-10 20:24:16 +0300

    Introduce abstract storage and sorted storage

--- src/ru/basejava/webapp/storage/AbstractArrayStorage.java
+++ src/ru/basejava/webapp/storage/AbstractArrayStorage.java
@@ -2,38 +2,21 @@ package ru.basejava.webapp.storage;
 
 import ru.basejava.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class AbstractArrayStorage implements Storage{
-    public static final int STORAGE_LIMIT = 10000;
-    private Resume[] storage = new Resume[STORAGE_LIMIT ];
-    private int size;
+public abstract class AbstractArrayStorage implements Storage{
+    protected static final int STORAGE_LIMIT = 10000;
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
+    protected Resume[] storage = new Resume[STORAGE_LIMIT ];
+    protected int size = 0;
+
+    public int size() {
+        return size;
     }
 
-    //save the new element after last element
-    public void save(Resume resume) {
-        if (checkResume(resume.getUuid()) >= 0) {
-            System.out.println("Error: storage already has this resume with uuid [" + resume.getUuid() + "]");
-            return;
-        }
-        if (size == storage.length) {
-            System.out.println("Exception: Array Index Out Of Bounds");
-            return;
-        }
-        storage[size] = resume;
-        size++;
-    }
-
-    //get the element
     public Resume get(String uuid) {
-        int index = checkResume(uuid);
+        int index = getIndex(uuid);
         if (index >= 0) {
             return storage[index];
         }
@@ -41,45 +24,5 @@ public class AbstractArrayStorage implements Storage{
         return null;
     }
 
-    //delete element and shift other elements one left
-    public void delete(String uuid) {
-        int index = checkResume(uuid);
-        if (index >= 0) {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        } else {
-            System.out.println("Error: resume with uuid [" + uuid + "] not found");
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
-    //not null element length
-    public int size() {
-        return size;
-    }
-
-    public void update(Resume resume) {
-        int index = checkResume(resume.getUuid());
-        if (index >= 0) {
-            storage[index] = resume;
-            return;
-        }
-        System.out.println("Error: resume with uuid [" + resume.getUuid() + "] not found");
-    }
-
-    private int checkResume(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+    protected abstract int getIndex(String uuid);
 }
--- src/ru/basejava/webapp/storage/ArrayStorage.java
+++ src/ru/basejava/webapp/storage/ArrayStorage.java
@@ -7,19 +7,15 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage{
-    public static final int STORAGE_LIMIT = 100000;
-    private Resume[] storage = new Resume[STORAGE_LIMIT ];
-    private int size;
+public class ArrayStorage extends AbstractArrayStorage{
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    //save the new element after last element
     public void save(Resume resume) {
-        if (checkResume(resume.getUuid()) >= 0) {
+        if (getIndex(resume.getUuid()) >= 0) {
             System.out.println("Error: storage already has this resume with uuid [" + resume.getUuid() + "]");
             return;
         }
@@ -31,19 +27,8 @@ public class ArrayStorage implements Storage{
         size++;
     }
 
-    //get the element
-    public Resume get(String uuid) {
-        int index = checkResume(uuid);
-        if (index >= 0) {
-            return storage[index];
-        }
-        System.out.println("Error: resume with uuid [" + uuid + "] not found");
-        return null;
-    }
-
-    //delete element and shift other elements one left
     public void delete(String uuid) {
-        int index = checkResume(uuid);
+        int index = getIndex(uuid);
         if (index >= 0) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
@@ -60,13 +45,8 @@ public class ArrayStorage implements Storage{
         return Arrays.copyOfRange(storage, 0, size);
     }
 
-    //not null element length
-    public int size() {
-        return size;
-    }
-
     public void update(Resume resume) {
-        int index = checkResume(resume.getUuid());
+        int index = getIndex(resume.getUuid());
         if (index >= 0) {
             storage[index] = resume;
             return;
@@ -74,7 +54,7 @@ public class ArrayStorage implements Storage{
         System.out.println("Error: resume with uuid [" + resume.getUuid() + "] not found");
     }
 
-    private int checkResume(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
