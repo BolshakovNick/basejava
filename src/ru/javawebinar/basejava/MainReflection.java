5a841d2a8ed4dc6cda92681b5e07d69b09dc4e03
Local <bolshakov_wrestling@icloud.com>
2020-07-16 16:40:52 +0300

    implementation toString with reflect

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -6,14 +6,16 @@ import java.lang.reflect.*;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
         System.out.println(r);
+        // TODO : invoke r.toString via reflection
+        Method method = r.getClass().getDeclaredMethod("toString", null);
+        System.out.println(method.invoke(r, null));
     }
 }
