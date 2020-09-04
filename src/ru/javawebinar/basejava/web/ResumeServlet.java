e246b35f3f35db260c84a9635706e70acdc7a2b5
Local <bolshakov_wrestling@icloud.com>
2020-09-04 16:01:35 +0300

    HW15 JSP

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.Config;
+import ru.javawebinar.basejava.storage.Storage;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,16 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ResumeServlet extends HttpServlet {
+    private final Storage storage = Config.get().getSqlStorage();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 //      response.setHeader("Content-Type", "text/html; charset=UTF-8");
         response.setContentType("text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello, " + name + '!');
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello, " + name + '!');
+        String uuid = request.getParameter("uuid");
+        request.setAttribute("resume", uuid == null ? null : storage.get(uuid));
+        request.setAttribute("storage", storage);
+        request.getRequestDispatcher("ResumePage.jsp");
     }
 }
