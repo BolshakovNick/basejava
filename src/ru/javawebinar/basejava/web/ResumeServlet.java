01698ee04ef2d9ad4e6b0fcd0f2712d7241f8f93
Local <bolshakov_wrestling@icloud.com>
2020-09-16 13:09:04 +0300

    HW16 add new Resume

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -28,45 +28,58 @@ public class ResumeServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         String uuid = request.getParameter("uuid");
         String fullName = request.getParameter("fullName");
-        final Resume r = storage.get(uuid);
+        String action = request.getParameter("action");
+        Resume r;
+        boolean isSaving = false;
+        if ("edit".equals(action)) {
+            r = storage.get(uuid);
+        } else {
+            isSaving = true;
+            r = new Resume("");
+            initEmptyResume(r);
+        }
         r.setFullName(fullName);
         for (ContactType type : ContactType.values()) {
             String value = request.getParameter(type.name());
             if (value != null && value.trim().length() != 0) {
-                r.addContact(type, value);
-            } else {
                 r.getContacts().remove(type);
+            } else {
+                r.addContact(type, value);
             }
         }
-
         for (SectionType type : SectionType.values()) {
             String value = request.getParameter(type.name());
-            if (value != null && value.trim().length() != 0) {
+            if (value == null || value.trim().length() == 0) {
+                r.getSections().remove(type);
+            } else {
                 switch (type) {
-                    case PERSONAL:
                     case OBJECTIVE:
+                    case PERSONAL:
                         r.addSection(type, new SimpleTextSection(value));
                         break;
                     case ACHIEVEMENT:
                     case QUALIFICATIONS:
-                        List<String> list = Arrays.stream(value.split("\n"))
+                        List<String> list = Arrays.stream(value.split("\\n"))
                                 .map(s -> "\r".equals(s) || " ".equals(s) || "\n".equals(s) ? s = "" : s)
                                 .collect(Collectors.toList());
                         r.addSection(type, new MarkingListSection(list));
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-
+                        break;
                 }
-            } else {
-                r.getContacts().remove(type);
             }
         }
-        storage.update(r);
-        response.sendRedirect("resume");
+        if (isSaving) {
+            storage.save(r);
+        } else {
+            storage.update(r);
+            response.sendRedirect("resume");
+        }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
         String uuid = request.getParameter("uuid");
         String action = request.getParameter("action");
         if (action == null) {
@@ -86,22 +99,7 @@ public class ResumeServlet extends HttpServlet {
                 break;
             case "add":
                 r = new Resume("");
-                for (SectionType type: SectionType.values()) {
-                    switch (type) {
-                        case PERSONAL:
-                        case OBJECTIVE:
-                            r.addSection(type, new SimpleTextSection(""));
-                            break;
-                        case QUALIFICATIONS:
-                        case ACHIEVEMENT:
-                            r.addSection(type, new MarkingListSection(new ArrayList<>()));
-                            break;
-                        case EXPERIENCE:
-                        case EDUCATION:
-                            r.addSection(type, new Organization(new Link("", null), new ArrayList<>()));
-                            break;
-                    }
-                }
+                initEmptyResume(r);
                 break;
             default:
                 throw new IllegalArgumentException("Action " + action + " is illegal");
@@ -112,49 +110,22 @@ public class ResumeServlet extends HttpServlet {
         ).forward(request, response);
     }
 
-    /*private static void printResume(Resume resume, Writer writer) throws IOException {
-        writer.write("<html>\n" +
-                " <head>\n" +
-                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-                "  <title>Resume Table</title>\n" +
-                " </head>\n" +
-                " <body>\n" +
-                "  <table border=\"1\" cellpadding=\"10\">\n" +
-                "   <tr>\n" +
-                "    <th>UUID</th>\n" +
-                "    <th>FULL_NAME</th>\n" +
-                "    <th>CONTACTS</th>\n" +
-                "    <th>CONTENT</th>\n" +
-                "   </tr>\n" +
-                "   <tr>\n" +
-                "    <td>" + resume.getUuid() + "</td>\n" +
-                "    <td>" + resume.getFullName() + "</td>\n");
-        String contacts = "";
-        for (Map.Entry<ContactType, String> entry : resume.getContacts().entrySet()) {
-            contacts = String.join("", contacts, entry.getKey().name(), ": ", entry.getValue(), "<br>");
-        }
-        writer.write("<td>" + contacts + "</td>\n");
-
-        String content = "";
-        for (Map.Entry<SectionType, AbstractSection> entry : resume.getSections().entrySet()) {
-            SectionType type = entry.getKey();
-            AbstractSection section = entry.getValue();
-            content = String.join("", content, entry.getKey().name(), "<br>");
-            if (type == SectionType.PERSONAL || type == SectionType.OBJECTIVE) {
-                content = String.join("", content, ((SimpleTextSection) section).getText(), "<br>");
-            } else if (type == SectionType.ACHIEVEMENT || type == SectionType.QUALIFICATIONS) {
-                List<String> lines = ((MarkingListSection) section).getMarkingLines();
-                for (String s : lines) {
-                    content = String.join("", content, " - ", s, "<br>");
-                }
+    private void initEmptyResume(Resume r) {
+        for (SectionType type : SectionType.values()) {
+            switch (type) {
+                case PERSONAL:
+                case OBJECTIVE:
+                    r.addSection(type, new SimpleTextSection(""));
+                    break;
+                case QUALIFICATIONS:
+                case ACHIEVEMENT:
+                    r.addSection(type, new MarkingListSection(new ArrayList<>()));
+                    break;
+                case EXPERIENCE:
+                case EDUCATION:
+                    r.addSection(type, new OrganizationSection(new ArrayList<>()));
+                    break;
             }
-            content = String.join("", content, "<br>");
         }
-        writer.write("<td>" + content + "</td>\n" +
-                "  </tr>\n" +
-                " </table>\n" +
-                " </body>\n" +
-                "</html>");
-    }*/
+    }
 }
